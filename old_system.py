d5e0aa194190ae9daa88afcd524930cf9474f538
Brendyn <bam30@kent.ac.uk>
2026-02-16 11:29:45 +0000

    Remove empty line in old_system.py
    Removed an empty line for cleaner code.

--- old_system.py
+++ old_system.py
@@ -1,7 +1,6 @@
 n = ["Picard", "Riker", "Data", "Worf"]
 r = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
 d = ["Command", "Command", "Operations", "Security"]
-
 active = True
 
 def run_system_monolith():
